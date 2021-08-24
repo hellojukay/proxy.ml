93864f2ffd44190f46a71340eee7ae9159970ee0
hellojukay <licong@qianxin.com>
2021-08-24 17:59:59 +0800

    日志带上时间戳

--- log.ml
+++ log.ml
@@ -1,5 +1,11 @@
+let format (tm : Unix.tm) =
+  Printf.sprintf "%d-%d-%d %d:%d:%d" tm.tm_year tm.tm_mon tm.tm_mday tm.tm_hour
+    tm.tm_min tm.tm_sec
+
 let info str =
-  Printf.printf "%s\n" str;
+  let tm = Unix.time () |> Unix.localtime in
+  let date_str = format tm in
+  Printf.printf "%s %s\n" date_str str;
   flush stdout
 
 let printf = Printf.printf
